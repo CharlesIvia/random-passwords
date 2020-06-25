57fb6d157e5436626373cd3fb658acbc138a7809
Charles Ivia <iviacharles@gmail.com>
2020-06-25 11:49:31 +0300

    Fetched dom elements and set variables

---
+++ script.js
@@ -0,0 +1,11 @@
+//Fetch dom elements and and set variables
+
+const result = document.querySelector("#result"),
+  copyBtn = document.querySelector(".result-copy"),
+  length = document.querySelector("#length"),
+  uppercase = document.querySelector("#uppercase"),
+  lowercase = document.querySelector("#lowercase"),
+  numbers = document.querySelector("#numbers"),
+  symbols = document.querySelector("#symbols"),
+  generate = document.querySelector("#generate"),
+  message = document.querySelector(".message");
