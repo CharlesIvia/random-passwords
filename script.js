169bb290ebe7ed84f1a34a12de7093169e1818e1
Charles Ivia <iviacharles@gmail.com>
2020-06-25 11:59:54 +0300

    Created logic to generate password.

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ const result = document.querySelector("#result"),
   symbols = document.querySelector("#symbols"),
   generate = document.querySelector("#generate"),
   message = document.querySelector(".message");
+
+//Fn to generate passsword
+
+generate.addEventListener("click", function (e) {
+  e.preventDefault();
+  let passswordLength = length.value;
+  if (passswordLength > 4 && passswordLength <= 25) {
+    let passsword = "";
+    for (let i = 0; i < passswordLength; i++) {
+      passsword += generateRandomCharacter();
+    }
+    result.value = passsword;
+    message.textContent = "";
+  } else {
+    message.textContent = "Selecr length betwen 5 and 25";
+    if (message.classList.contains("message-success")) {
+      message.classList.remove("message-success");
+    }
+    result.value = "";
+    hideMessage();
+  }
+});
