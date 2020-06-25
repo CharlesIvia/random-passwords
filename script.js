9ec10860712d7e42cb0d4f8c1b078dbee91da499
Charles Ivia <iviacharles@gmail.com>
2020-06-25 12:48:38 +0300

    Run random fn if symbols checkbox is not checked

--- script.js
+++ script.js
@@ -106,4 +106,19 @@ function generateRandomCharacter() {
 
     return functions[Math.floor(Math.random() * 4)]();
   }
+
+  if (
+    uppercase.checked &&
+    lowercase.checked &&
+    numbers.checked &&
+    !symbols.checked
+  ) {
+    const functions = {
+      0: getRandomUppercase,
+      1: getRandomLowercase,
+      2: getRandomNumber,
+    };
+
+    return functions[Math.floor(Math.random() * 3)]();
+  }
 }
